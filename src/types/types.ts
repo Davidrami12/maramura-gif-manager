9c217b58ad65e1c59b5d48db8c8c955d2484f689
David R. <davidrami2000@gmail.com>
2025-03-20 01:32:58 +0100

    feat(frontend): :label: Update new types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,20 @@
+export type Gif = {
+  id: string;
+  gif: {
+    src: string;
+    title: string;
+  };
+}
+
+export type GifGalleryProps = {
+  gifs: Gif[];
+}
+
 export type FileUploaderProps = {
   onUpload: (files: File[]) => void;
-};
+}
 
 export type GifGeneratorProps = {
   imageUrls: string[];
-};
-  
+  setGifs: (gifs: Gif[]) => void;
+}
